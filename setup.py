5054f0dd1fbadd5e243d33648bee602ab738b7b8
Ted Ralphs <ted@lehigh.edu>
2019-03-24 20:16:00 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from setuptools import setup, Extension
 import os
+import numpy
 
 cdd_dir = 'cddlib-094d-p1'
 sources = ['cddcore.c','cddlp.c','cddmp.c','cddio.c',
            'cddlib.c','cddproj.c','setoper.c']
 cdd_sources = [os.path.join(cdd_dir,'lib-src',fn) for fn in sources]
 cdd_sources.append(os.path.join('src','_cddmodule.c'))
-include_dirs = [os.path.join(cdd_dir,'lib-src')]
+include_dirs = [os.path.join(cdd_dir,'lib-src'), numpy.get_include()]
 
 modules=[Extension('_cdd', 
                    cdd_sources, 
